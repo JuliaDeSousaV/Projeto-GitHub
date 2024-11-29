56df25dad58b05e5ad97ad9849fca3352707d7c6
JuliaDeSousaV <venancio.julia@escola.pr.gov.br>
2024-11-29 19:15:05 -0300

    movie.js

---
+++ movie.js
@@ -0,0 +1,174 @@
+export const movies = [
+  {
+    title: "Uma Noite no Museu",
+    genre: "Comédia",
+    ageRating: "Livre",
+    synopsis: "O novo segurança do Museu de História Natural de Nova York faz uma grande descoberta. Por causa de uma antiga maldição egípcia, os animais, pássaros, insetos e outras peças em exibição ganham vida quando os visitantes vão embora. Soldados romanos, pistoleiros do velho oeste, o ex-presidente norte-americano Theodore Roosevelt e muitas outras figuras históricas entram em ação em uma aventura fantástica.",
+    trailer: "Uma Noite no Museu  Trailer"
+  },
+  {
+    title: "0 Auto da Compadecida",
+    genre: "Comédia",
+    ageRating: "10",
+    synopsis: "As aventuras de João Grilo e Chicó, dois nordestinos pobres que vivem de golpes para sobreviver. Eles estão sempre enganando o povo de um pequeno vilarejo, inclusive o temido cangaceiro Severino de Aracaju, que os persegue pela região.",
+    trailer: "O AUTO DA COMPADECIDA 2000 Trailer"
+  },
+  {
+    title: "Todo Mundo Quase Morto",
+    genre: "Comédia",
+    ageRating: "14",
+    synopsis: "Um funcionário de vendas de uma loja de eletrônicos e seu melhor amigo precisam salvar seus amigos e suas famílias de zumbis que tomaram conta de Londres.",
+    trailer: "Shaun of the Dead - 20th Anniversary Release Official Trailer - Only in Theaters August 30"
+  },
+  {
+    title: "Todo Mundo em Pânico",
+    genre: "Comédia",
+    ageRating: "16",
+    synopsis: "Depois do assassinato de uma bela colega de classe, um grupo de adolescentes desorientados descobre que há um assassino entre eles. A heroína Cindy Campbell e a sua turma de amigos tentam se proteger do perigo, mas Gail Hailstorm, uma repórter irritante, simplesmente não os deixa em paz.",
+    trailer: "Todo Mundo em Pânico - trailer legendado"
+  },
+  {
+    title: "Forrest Gump",
+    genre: "Comédia",
+    ageRating: "14",
+    synopsis: "Mesmo com o raciocínio lento, Forrest Gump nunca se sentiu desfavorecido. Graças ao apoio da mãe, ele teve uma vida normal. Seja no campo de futebol como um astro do esporte, lutando no Vietnã ou como capitão de um barco de pesca de camarão, Forrest inspira a todos com seu otimismo. Mas a pessoa que Forrest mais ama pode ser a mais difícil de salvar: seu amor de infância, a doce e perturbada Jenny.",
+    trailer: "Forrest Gump - Trailer"
+  },
+  {
+    title: "Mulan",
+    genre: "Ação",
+    ageRating: "Livre",
+    synopsis: "Mulan, uma jovem chinesa que não se encaixa na sociedade, teme que seu pai, um homem doente, seja convocado para lutar na guerra que se aproxima. A garota então se disfarça de homem e assume o posto de seu pai no exército chinês. Acompanhada por seu dragão Mushu, Mulan parte para a linha de batalha, faz amizade com os outros soldados e usa sua inteligência para ajudar a combater a invasão dos hunos enquanto luta para esconder sua verdadeira identidade.",
+    trailer: "Mulan (1998) Trailer #1 | Movieclips Classic Trailers"
+  },
+  {
+    title: "A Lenda dos Guardiões",
+    genre: "Ação",
+    ageRating: "10",
+    synopsis: "As histórias épicas contadas pelo pai sobre os Guardiões de GaHoole encantam seu filho Soren, mas o filho mais velho ridiculariza as histórias dos guerreiros alados que travaram uma batalha épica para salvar a todos dos malvados Puros. Os irmãos são capturados pelos Puros, mas Soren consegue fugir e, com a ajuda das corujas jovens, procura os Guardiões e os traz de volta para defender novamente seu povo.",
+    trailer: "A Lenda dos Guardiões - Trailer Teaser (dublado)"
+  },
+  {
+    title: "Os Vingadores",
+    genre: "Ação",
+    ageRating: "12",
+    synopsis: "Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.",
+    trailer: "Os Vingadores: The Avengers- Trailer Oficial - Dublado"
+  },
+  {
+    title: "Batman O Cavaleiro das Trevas",
+    genre: "Ação",
+    ageRating: "14",
+    synopsis: "Batman tem conseguido manter a ordem em Gotham com a ajuda de Jim Gordon e Harvey Dent. No entanto, um jovem e anárquico criminoso, conhecido apenas como Coringa, pretende testar o Cavaleiro das Trevas e mergulhar a cidade em um verdadeiro caos.",
+    trailer: ""
+  },
+  {
+    title: "John Wick – De Volta ao Jogo",
+    genre: "Ação",
+    ageRating: "16",
+    synopsis: "John Wick é um lendário assassino de aluguel aposentado, lidando com o luto após perder o grande amor de sua vida. Quando um gângster invade sua casa, mata seu cachorro e rouba seu carro, ele é forçado a voltar à ativa e inicia sua vingança.",
+    trailer: "De Volta ao Jogo Trailer Oficial Legendado (2014) - Keanu Reeves HD"
+  },
+  {
+    title: "Kung Fu Panda",
+    genre: "Animação",
+    ageRating: "Livre",
+    synopsis: "Po é um panda que trabalha na loja de macarrão da sua família e sonha em transformar-se em um mestre de kung fu. Seu sonho se torna realidade quando, inesperadamente, ele deve cumprir uma profecia antiga e estudar a arte marcial com seus ídolos, os Cinco Furiosos. Po precisa de toda a sabedoria, força e habilidade que conseguir reunir para proteger seu povo de um leopardo da neve malvado.",
+    trailer: "https://youtu.be/jZzvRsU-swo?feature=shared"
+  },
+  {
+    title: "Homem-Aranha: Através do Aranhaverso",
+    genre: "Animação",
+    ageRating: "10",
+    synopsis: "Depois de se reunir com Gwen Stacy, Homem-Aranha é jogado no multiverso. Lá, o super-herói aracnídeo encontra uma numerosa equipe encarregada de proteger sua própria existência.",
+    trailer: "https://youtu.be/uhkDkurK0Zg?si=7QL1pOaDTR3WRKkG"
+  },
+  {
+    title: "O Fantasma do Futuro",
+    genre: "Animação",
+    ageRating: "14",
+    synopsis: "Ghost in The Shell apresenta uma trama onde o mundo se tornou um local altamente informatizado, a ponto dos seres humanos poderem acessar extensas redes de informações com seus ciber-cérebros. A agente cibernética Major Motoko é a líder da unidade de serviço secreto Esquadrão Shell, combatente do crime.",
+    trailer: "https://youtu.be/8RF09G8Ymqg?si=_jW_S0YVVULy7xG2"
+  },
+  {
+    title: "Perfect Blue",
+    genre: "Animação",
+    ageRating: "16",
+    synopsis: "Mima Kirigoe é uma cantora pop de uma banda CHAM!, mas decide se tornar uma atriz, tendo como primeiro projeto uma série de crime dramática. Muitos de seus fãs ficam chateados com sua decisão e uns deles, obcecado por Mima, começa a persegui-la e a enviar mensagens a chamando de traidora.",
+    trailer: "https://youtu.be/2O09gQC-KvA?si=T3tVLCUkskfNh_zO"
+  },
+  {
+    title: "A Vida Secreta de Walter Mitty (2013)",
+    genre: "Aventura",
+    ageRating: "10",
+    synopsis: "Walter Mitty desenvolve fotos para uma revista, mas foge do tédio através de devaneios heroicos. Quando Walter recebe uma chance para uma verdadeira aventura, ele tem a chance de construir um relacionamento com uma colega de trabalho chamada Cheryl.",
+    trailer: "https://youtu.be/U4fCSQO1PeA?si=7tHrRwZzhZGCIl7-"
+  },
+ {
+{ 
+title: "Gato de Botas 2: O Último Pedido", 
+genre: "Aventura", 
+ageRating: Livre, 
+synopsis: "O Gato de Botas descobre que sua paixão pela aventura cobrou seu preço: ele já gastou oito de suas nove vidas. Ele então parte em uma jornada épica para encontrar o mítico Último Desejo e restaurar suas nove vidas.", 
+trailer: "Puss In Boots: The Last Wish - Official Trailer 2" 
+}, 
+{ 
+title: "Senhor Dos Anéis, 
+genre: "Aventura", 
+        ageRating: 12, 
+synopsis: "Em uma terra fantástica e única, um hobbit recebe de presente de seu tio um anel mágico e maligno que precisa ser destruído antes que caia nas mãos do mal. Para isso, o hobbit Frodo tem um caminho árduo pela frente, onde encontra perigo, medo e seres bizarros. Ao seu lado para o cumprimento desta jornada, ele aos poucos pode contar com outros hobbits, um elfo, um anão, dois humanos e um mago, totalizando nove seres que formam a Sociedade do Anel.", 
+trailer: "O Senhor dos Anéis: Os Anéis de Poder | Trailer Comic-Con San Diego | Prime Video" 
+}, 
+
+{ 
+title: "Gladiador", 
+genre: "Aventura", 
+ageRating: 14, 
+synopsis: "Maximus é um poderoso general romano, amado pelo povo e pelo imperador Marcus Aurelius. Antes de sua morte, o Imperador desperta a ira de seu filho Commodus ao tornar pública a sua predileção em deixar o trono para Maximus. Sedento pelo poder, Commodus mata seu pai, assume a coroa e ordena a morte de Maximus, que consegue fugir antes de ser pego, e passa a se esconder como um escravo e gladiador enquanto vai atrás de vingança.", 
+trailer: "GLADIATOR | Official Trailer | Paramount Movies" 
+}, 
+{ 
+title: "Os Mercenários",
+genre: "Aventura", 
+ageRating: 16, 
+synopsis: "Um grupo de mercenários, liderado por Barney Ross e composto de seus homens de confiança, aceita uma missão que parece ser rotineira: uma operação secreta, a mando da Igreja, para invadir o país sul-americano de Vilena e derrubar seu ditador. Não demora muito para o grupo perceber que a missão é uma operação suicida. Presos em uma perigosa teia de traições, mas sabendo que um inocente está em perigo, Barney e seus homens decidem finalizar o trabalho.", 
+trailer: "The Expendables (2010) - Official Trailer #1" 
+}, 
+{ 
+title: "E.T. O Extraterrestre", 
+genre: "Ficção científica", 
+ageRating: Livre, 
+synopsis: "O garoto Elliott faz amizade com um pequeno alienígena inofensivo que está bem longe do seu planeta. Ele decide manter a adorável criatura em segredo e em casa após apresentá-la aos irmãos.", 
+trailer: ""E.T. O EXTRA-TERRESTRE" - Trailer Oficial Legendado (Universal Pictures Portugal)" 
+}, 
+{ 
+title: "Guardiões da Galáxia", 
+genre: "Ficção científica", 
+ageRating: 12, 
+synopsis: "O aventureiro do espaço Peter Quill torna-se presa de caçadores de recompensas depois que rouba a esfera de um vilão traiçoeiro, Ronan. Para escapar do perigo, ele faz uma aliança com um grupo de quatro extraterrestres. Quando Quill descobre que a esfera roubada possui um poder capaz de mudar os rumos do universo, ele e seu grupo deverão proteger o objeto para salvar o futuro da galáxia.", 
+trailer: "Trailer - Guardiões da Galáxia" 
+}, 
+{ 
+title: "Paprika", 
+genre: "Ficção científica", 
+ageRating: 12, 
+synopsis: "Atsuko Chiba é a psiquiatra responsável por conduzir um tratamento experimental através do qual os médicos conseguem entrar nos sonhos dos pacientes. No entanto, ela vai mais longe para auxiliá-los e começa a usar o método de forma ilegal, adotando o pseudônimo de Paprika", 
+trailer: "PAPRIKA [2007] - Official Trailer (HD)" 
+}, 
+{ 
+title: "Alien, o 8.º Passageiro", 
+genre: "Ficção científica ", 
+ageRating: 14, 
+synopsis: "Uma nave espacial, ao retornar para Terra, recebe estranhos sinais vindos de um asteroide. Enquanto a equipe investiga o local, um dos tripulantes é atacado por um misterioso ser. O que parecia ser um ataque isolado se transforma em um terror constante, pois o tripulante atacado levou para dentro da nave o embrião de um alienígena, que não para de crescer e tem como meta matar toda a tripulação.
+
+", 
+trailer: "Alien Trailer HD (Original 1979 Ridley Scott Film) Sigourney Weaver" 
+}, 
+{ 
+title: "O Paradoxo Cloverfield", 
+genre: "Ficção científica", 
+ageRating: 16, 
+synopsis: "A tripulação de uma estação espacial testa um dispositivo que pode gerar energia ilimitada, mas que corre o risco de criar um terrível paradoxo, deixando-os em uma sombria realidade alternativa.", 
+trailer: "THE CLOVERFIELD PARADOX | SE DEN NU | NETFLIX | DK" 
+}, 
+];
